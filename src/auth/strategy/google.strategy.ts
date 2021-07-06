26594d22576dca777d00cb1d94a1cadb6fd75140
sonchaegeon <thscorjs1002@naver.com>
2021-07-06 15:42:19 +0900

    📑 :: google strategy

---
+++ src/auth/strategy/google.strategy.ts
@@ -0,0 +1,32 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { Injectable } from '@nestjs/common';
+import 'dotenv/config';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor() {
+    super({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const { displayName, emails, photos } = profile;
+    const user = {
+      sub: 0,
+      email: emails[0].value,
+      name: displayName,
+      profile_url: photos[0].value,
+    };
+    done(null, user);
+  }
+}
