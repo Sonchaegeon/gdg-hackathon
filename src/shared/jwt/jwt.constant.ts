92fc2b948e6c77e09fce8e53b23ae587d12162d4
sonchaegeon <thscorjs1002@naver.com>
2021-07-06 15:39:30 +0900

    📑 :: jwt constant

---
+++ src/shared/jwt/jwt.constant.ts
@@ -0,0 +1,6 @@
+import 'dotenv/config';
+
+export const ACCESS_TOKEN_EXPIRED_TIME = process.env.ACCESS_TOKEN_EXPIRED_TIME;
+export const REFRESH_TOKEN_EXPIRED_TIME =
+  process.env.REFRESH_TOKEN_EXPIRED_TIME;
+export const JWT_SECRET_KEY = process.env.JWT_SECRET;
