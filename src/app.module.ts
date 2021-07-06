590f96fc471fd632b0b78e1ca437d416dd06ddef
sonchaegeon <thscorjs1002@naver.com>
2021-07-06 15:38:49 +0900

    ⚡️ :: import typeorm, auth module, jwt module. provide httperror filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmConfigModule } from './typeorm/typeorm-config.module';
+import { JwtModule } from '@nestjs/jwt';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpErrorFilter } from './shared/exception/exception.filter';
 
 @Module({
-  imports: [],
-  providers: [EventsGateway],
+  imports: [TypeOrmConfigModule, AuthModule, JwtModule.register({})],
+  providers: [
+    EventsGateway,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+  ],
 })
 export class AppModule {}
