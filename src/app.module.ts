11f55f5f5304ae7b72352bd52fcd24096cb4c5d4
sonchaegeon <thscorjs1002@naver.com>
2021-07-08 11:17:27 +0900

    ⚡️ :: import post module, provide jwt strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,19 @@ import { TypeOrmConfigModule } from './typeorm/typeorm-config.module';
 import { JwtModule } from '@nestjs/jwt';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpErrorFilter } from './shared/exception/exception.filter';
+import { PostModule } from './post/post.module';
+import { JwtStrategy } from './shared/jwt/strategy/jwt.strategy';
 
 @Module({
-  imports: [TypeOrmConfigModule, AuthModule, JwtModule.register({})],
+  imports: [
+    TypeOrmConfigModule,
+    AuthModule,
+    JwtModule.register({}),
+    PostModule,
+  ],
   providers: [
     EventsGateway,
+    JwtStrategy,
     {
       provide: APP_FILTER,
       useClass: HttpErrorFilter,
