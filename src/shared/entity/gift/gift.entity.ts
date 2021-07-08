1cd142c9515fa11c6c071583dbcab4f0ebb7adb4
sonchaegeon <thscorjs1002@naver.com>
2021-07-08 11:19:28 +0900

    📑 :: gift entity

---
+++ src/shared/entity/gift/gift.entity.ts
@@ -0,0 +1,39 @@
+import { Post } from '../../../post/entity/post.entity';
+import { Shop } from '../../../shared/entity/shop/shop.entity';
+import { User } from '../../../shared/entity/user/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { GiftImage } from './gift-image.entity';
+
+@Entity('gift')
+export class Gift {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 150 })
+  name: string;
+
+  @Column({ type: 'int' })
+  price: number;
+
+  @OneToMany(() => GiftImage, (giftImage) => giftImage.gift_id)
+  gift_image: string;
+
+  @OneToOne(() => Shop, (shop) => shop.gift)
+  @JoinColumn({ name: 'shop_id' })
+  shop: number;
+
+  @ManyToOne(() => User, (user) => user.gift)
+  @JoinColumn({ name: 'user_id' })
+  user: number;
+
+  @OneToOne(() => Post, (post) => post.gift)
+  post: Post;
+}
