29c5a85822a52a27037d59f6d2a290202909de1e
sonchaegeon <thscorjs1002@naver.com>
2021-07-09 09:08:19 +0900

    ⚰️ :: useless columns

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,20 +1,11 @@
-import { IsDateString, IsNumber } from 'class-validator';
-import { Post } from '../entity/post.entity';
+import { IsNumber } from 'class-validator';
 
 export class CreatePostDto {
-  @IsDateString()
-  exp_date: Date;
-
-  @IsNumber()
-  price: number;
-
   @IsNumber()
   gift_id: number;
 }
 
 export class CreatePostResponseData {
-  exp_date: Date;
-  price: number;
   user: number;
   gift: number;
   id: number;
