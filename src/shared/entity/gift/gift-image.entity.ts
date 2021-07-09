8c01bc7e798beb91f3cc774e46969435eae19fef
sonchaegeon <thscorjs1002@naver.com>
2021-07-09 09:07:12 +0900

    ⚡️ :: add column full_url, cover_url
    modify relation one to one with gift entity

--- src/shared/entity/gift/gift-image.entity.ts
+++ src/shared/entity/gift/gift-image.entity.ts
@@ -1,13 +1,16 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm';
 import { Gift } from './gift.entity';
 
 @Entity('gift_image')
 export class GiftImage {
   @PrimaryColumn()
-  @ManyToOne(() => Gift, (gift) => gift.gift_image)
+  @OneToOne(() => Gift, (gift) => gift.gift_image)
   @JoinColumn({ name: 'gift_id' })
   gift_id: number;
 
   @Column({ length: 150 })
-  url: string;
+  cover_url: string;
+
+  @Column({ length: 150 })
+  full_url: string;
 }
