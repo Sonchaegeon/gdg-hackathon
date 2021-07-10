d98235d36fff925815e0c0209a0437d338765aef
sonchaegeon <thscorjs1002@naver.com>
2021-07-10 20:44:34 +0900

    ⚡️ :: socket email set

--- src/shared/jwt/guard/ws.guard.ts
+++ src/shared/jwt/guard/ws.guard.ts
@@ -5,7 +5,7 @@ import {
   Logger,
 } from '@nestjs/common';
 import { WsException } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { SocketType } from 'src/events/interface/socket-type.interface';
 import { AuthService } from '../../../auth/auth.service';
 
 @Injectable()
@@ -14,11 +14,13 @@ export class WsJwtGuard implements CanActivate {
   private logger = new Logger('WsJwtGuard');
   async canActivate(context: ExecutionContext): Promise<boolean> {
     try {
-      const client: Socket = context.switchToWs().getClient<Socket>();
+      const client: SocketType = context.switchToWs().getClient<SocketType>();
       const token: string = client.handshake.query.token as string;
       const user = await this.authService.verifyUser(token);
-      context.switchToHttp().getRequest().user = user;
 
+      client.email = user.email;
+
+      context.switchToHttp().getRequest().user = user;
       return Boolean(user);
     } catch (err) {
       this.logger.error(err.message);
