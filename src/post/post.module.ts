ecfa27f7d64033bcf7435232a94a598bee73e10a
sonchaegeon <thscorjs1002@naver.com>
2021-07-10 13:35:59 +0900

    ⚡️ :: import post view repository

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -2,13 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GiftRepository } from '../shared/entity/gift/gift.repository';
 import { UserRepository } from '../shared/entity/user/user.repository';
+import { PostViewRepository } from './entity/post-view.repository';
 import { PostRepository } from './entity/post.repository';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([PostRepository, UserRepository, GiftRepository]),
+    TypeOrmModule.forFeature([
+      PostRepository,
+      UserRepository,
+      GiftRepository,
+      PostViewRepository,
+    ]),
   ],
   controllers: [PostController],
   providers: [PostService],
