8068456b765dc9218162654735b0efe0771a7199
sonchaegeon <thscorjs1002@naver.com>
2021-07-10 13:32:49 +0900

    ⚡️ :: get applicants service

--- src/applicant/applicant.service.ts
+++ src/applicant/applicant.service.ts
@@ -8,11 +8,15 @@ import { Applicant } from './entity/applicant.entity';
 import { ApplicantRepository } from './entity/applicant.repository';
 import {
   ApplicantForbiddenException,
+  ApplicantNotFoundException,
   UserNotFoundException,
 } from '../shared/exception/exception.index';
 import { v4 } from 'uuid';
 import { Post } from '../post/entity/post.entity';
 import { PostRepository } from '../post/entity/post.repository';
+import { GetApplicantResponseData } from './dto/get-applicant.dto';
+import { PostView } from 'src/post/entity/post-view.entity';
+import { PostViewRepository } from 'src/post/entity/post-view.repository';
 
 @Injectable({ scope: Scope.REQUEST })
 export class ApplicantService {
@@ -21,11 +25,12 @@ export class ApplicantService {
     private readonly applicantRepository: ApplicantRepository,
     @InjectRepository(User) private readonly userRepository: UserRepository,
     @InjectRepository(Post) private readonly postRepository: PostRepository,
+    @InjectRepository(PostView)
+    private readonly postViewRepository: PostViewRepository,
     @Inject(REQUEST) private readonly req: IUserRequest,
   ) {}
 
   public async createApplicant(post_id: number): Promise<Applicant> {
-    console.log(this.req.user);
     const userRecord = await this.userRepository.findUserByEmail(
       this.req.user.email,
     );
@@ -37,5 +42,13 @@ export class ApplicantService {
       post_id,
       v4(),
     );
+  }
+
+  public async getApplicants(): Promise<GetApplicantResponseData[]> {
+    const applicantRecords = await this.postViewRepository.getApplicants(
+      this.req.user.email,
+    );
+    if (applicantRecords.length === 0) throw ApplicantNotFoundException;
+    return applicantRecords;
   }
 }
