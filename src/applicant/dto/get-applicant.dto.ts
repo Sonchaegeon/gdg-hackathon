b314a8f92a0555d4fbcbf68c580b87caacad716f
sonchaegeon <thscorjs1002@naver.com>
2021-07-10 13:32:13 +0900

    📑 :: get applicant dto

---
+++ src/applicant/dto/get-applicant.dto.ts
@@ -0,0 +1,13 @@
+export class GetApplicantResponseData {
+  post_id: number;
+  shop_name: string;
+  gift_name: string;
+  gift_image: string;
+  exp_date: Date;
+  price: number;
+  genre: string;
+  is_accept: number;
+  room_secret: string;
+  user_email: string;
+  post_email: string;
+}
